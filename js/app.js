6819508707d7b22586c4885c12a0d536f20e6e3d
Henry Acosta <acosta.henry@gmail.com>
2016-06-14 15:32:22 -0400

    basic-structure

---
+++ js/app.js
@@ -0,0 +1,5 @@
+ $(document).ready(function () {
+     // 1. get input from the user
+     // 2. getting input from the user make the api call.
+     // 3. useing the api call, and show the results to the user.
+ });
